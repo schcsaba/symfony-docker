ab3a71b3fa4afaceda24612d801627a5288f2874
Csaba Schnitchen <schcsaba@gmail.com>
2023-11-23 21:25:09 +0100

    define routes in attributes

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -3,11 +3,21 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController
 {
+    private array $messages = ['Hello', 'Hi', 'Bye'];
+
+    #[Route('/', name: 'app_index')]
     public function index(): Response
     {
-        return new Response('Hi!');
+        return new Response(implode(', ', $this->messages));
+    }
+
+    #[Route('messages/{id}', name: 'app_show_one')]
+    public function showOne($id): Response
+    {
+        return new Response($this->messages[$id]);
     }
 }
