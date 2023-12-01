3c1dfdf2406e8d0dc5d6264d74a526f3828f80d8
Csaba Schnitchen <schcsaba@gmail.com>
2023-12-01 17:57:52 +0100

    start using twig

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,19 @@
+
+8
+Makefile,8/3/836efb6e25a091dcb4ff8e1dbb2f0be6a5cbf14c
+R
+"src/Controller/HelloController.php,b/1/b1e2f8c98b96642f0600ab3884accf306caa9d77
+<
+compose.yaml,2/7/2701071a1490f4576d914b0b84bc3185e0aa2854
+R
+"templates/hello/show_one.html.twig,0/d/0d0ce3e897bee930cfa73596f8e2476142489772
+:
+
+Dockerfile,6/6/6651ddff6eb82c840ced7c1dddee15c6e1913dd4
+B
+config/routes.yaml,b/a/baef02989dedf0ec859f7bce087a69f29bef2b72
+O
+templates/hello/index.html.twig,6/3/63520923d7a3acbc31bb752bd93c660b5038a6b5
+:
+
+.gitignore,a/5/a5cc2925ca8258af241be7e5b0381edf30266302
--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -2,22 +2,33 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class HelloController
+class HelloController extends AbstractController
 {
     private array $messages = ['Hello', 'Hi', 'Bye'];
 
-    #[Route('/', name: 'app_index')]
-    public function index(): Response
+    #[Route('/{limit<\d+>?3}', name: 'app_index')]
+    public function index(int $limit): Response
     {
-        return new Response(implode(', ', $this->messages));
+        return $this->render(
+            'hello/index.html.twig',
+            [
+                'message' => implode(', ', array_slice($this->messages, 0, $limit))
+            ]
+        );
     }
 
-    #[Route('messages/{id}', name: 'app_show_one')]
-    public function showOne($id): Response
+    #[Route('messages/{id<\d+>}', name: 'app_show_one')]
+    public function showOne(int $id): Response
     {
-        return new Response($this->messages[$id]);
+        return $this->render(
+            'hello/show_one.html.twig',
+            [
+                'message' => $this->messages[$id]
+            ]
+        );
     }
 }
