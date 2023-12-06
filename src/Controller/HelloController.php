cdc289def35fd59bbcbf424111ccdb6c92a750db
Csaba Schnitchen <schcsaba@gmail.com>
2023-12-06 07:29:28 +0100

    use a partial template

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -8,7 +8,11 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class HelloController extends AbstractController
 {
-    private array $messages = ['Hello', 'Hi', 'Bye'];
+    private array $messages = [
+        ['message' => 'Hello', 'created' => '2023/11/12'],
+        ['message' => 'Hi', 'created' => '2023/10/12'],
+        ['message' => 'Bye!', 'created' => '2021/05/12']
+    ];
 
     #[Route('/{limit<\d+>?3}', name: 'app_index')]
     public function index(int $limit): Response
